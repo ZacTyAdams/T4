a5befcb794e873b57b9f67419e9c68cc0eb47e92
ZacAdams <ZacAtk@dynamic-10-124-28-216.dynamic.unt.edu>
2014-10-29 20:21:32 -0500

    Added the icon and updated the title
    Prototype fully functional

--- TapTwistTunes/gen/com/example/taptwisttunes/R.java
+++ TapTwistTunes/gen/com/example/taptwisttunes/R.java
@@ -1079,6 +1079,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int end=0x7f020057;
         public static final int ic_launcher=0x7f020058;
+        public static final int iconn=0x7f020059;
     }
     public static final class id {
         public static final int Title=0x7f06003e;
