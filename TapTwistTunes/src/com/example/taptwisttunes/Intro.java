5f1ab6c46bb1c6b5f9e8f0548abcefee36257a9d
ZacAdams <zta104@gmail.com>
2014-11-29 22:12:51 -0600

    Tarsos added, functionality awaiting

--- TapTwistTunes/src/com/example/taptwisttunes/Intro.java
+++ TapTwistTunes/src/com/example/taptwisttunes/Intro.java
@@ -1,8 +1,18 @@
 package com.example.taptwisttunes;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.WaveformSimilarityBasedOverlapAdd;
+import be.tarsos.dsp.WaveformSimilarityBasedOverlapAdd.Parameters;
+import be.tarsos.dsp.io.TarsosDSPAudioFormat;
+import be.tarsos.dsp.io.UniversalAudioInputStream;
+import be.tarsos.dsp.resample.RateTransposer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,6 +28,7 @@ public class Intro extends ActionBarActivity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.intro);
+		
 	}
 
 	public void gotoFileBrowser(View view) {
